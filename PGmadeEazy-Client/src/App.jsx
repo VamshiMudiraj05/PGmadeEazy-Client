069d65307e48994f9a045e3a77301e9bb028e4ac
jayadheer27 <Jayadheerreddypagadala27@gmail.com>
2025-02-13 19:28:12 +0530

    Footer with Back to Top button

--- PGmadeEazy-Client/src/App.jsx
+++ PGmadeEazy-Client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header";
 import AppRoutes from "./Routes/AppRoutes";
-
+import Footer from "./components/Footer";
 
 
 export default function App() {
@@ -10,7 +10,7 @@ export default function App() {
       <div className="min-h-screen flex flex-col">
         <Header />
         <AppRoutes />
-       
+        <Footer/>
       </div>
     </Router>
   );
