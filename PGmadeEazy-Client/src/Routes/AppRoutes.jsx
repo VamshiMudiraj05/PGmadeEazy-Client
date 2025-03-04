800f9f3786939262bc9ec27124feea502c08ca76
vamshimudiraj05 <vmudiraj330@gmail.com>
2025-03-04 09:13:14 +0530

    Added Hamburger Menu

--- PGmadeEazy-Client/src/Routes/AppRoutes.jsx
+++ PGmadeEazy-Client/src/Routes/AppRoutes.jsx
@@ -24,6 +24,7 @@ export default function AppRoutes() {
         <Route path="/profile" element={<Profile />} />
        <Route path="/seeker-dashboard" element={<SeekerDashboard />} />
         <Route path="/provider-dashboard" element={<ProviderDashboard />} />
+        
       </Routes>
     </>
   );
